b75112b930e642653a7e5b190e03e1880e40290e
Mateusz Samek <samekmat@gmail.com>
2023-03-13 16:43:34 +0100

    Create absoulte url for statistic model and create js script directory

--- stats/models.py
+++ stats/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Statistic(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse("stats:dashboard", kwargs={"slug": self.slug})
 
     @property
     def data(self):
